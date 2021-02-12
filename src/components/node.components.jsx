5e70fba82bad8c22b4138820ecc2bdb3aa7877d6
Dayanidhee Singh <43542454+Dayasingh22@users.noreply.github.com>
2021-02-12 00:30:05 -0800

    Added different style for the node

--- src/components/node.components.jsx
+++ src/components/node.components.jsx
@@ -5,6 +5,7 @@ import ReactFlow, {
   Background,
   Controls,
   MiniMap,
+  removeElements,
 } from "react-flow-renderer";
 
 const initialElements = [
@@ -24,7 +25,7 @@ const Test = () => {
   const [elements, setElements] = useState(initialElements);
   const [name, setName] = useState("");
 
-  const addNode = () => {
+  const addCircleNode = () => {
     setElements((e) =>
       e.concat({
         id: (e.length + 1).toString(),
@@ -37,40 +38,113 @@ const Test = () => {
         style: {
           background: "#D6D5E6",
           color: "#333",
+          wordWrap: "break-word",
           border: "1px solid #222138",
           width: 50,
-          height: 50,
           borderRadius: 50,
         },
       })
     );
   };
 
+  const addRectangleNode = () => {
+    setElements((e) =>
+      e.concat({
+        id: (e.length + 1).toString(),
+        data: { label: `${name}` },
+        animated: true,
+        position: {
+          x: Math.random() * window.innerWidth,
+          y: Math.random() * window.innerHeight,
+        },
+        style: {
+          background: "#098058",
+          color: "#333",
+          wordWrap: "break-word",
+          border: "1px solid #222138",
+        },
+      })
+    );
+  };
+
+  const addSquareNode = () => {
+    setElements((e) =>
+      e.concat({
+        id: (e.length + 1).toString(),
+        data: { label: `${name}` },
+        animated: true,
+        position: {
+          x: Math.random() * window.innerWidth,
+          y: Math.random() * window.innerHeight,
+        },
+        style: {
+          background: "#07cfc5",
+          color: "#333",
+          wordWrap: "break-word",
+          border: "1px solid #222138",
+          width: 50,
+        },
+      })
+    );
+  };
+
   const onConnect = (params) => setElements((e) => addEdge(params, e));
+  const onElementsRemove = (elementsToRemove) =>
+    setElements((els) => removeElements(elementsToRemove, els));
 
   return (
     <Fragment>
       <div className="Add">
-        <input
-          type="text"
-          onChange={(e) => setName(e.target.value)}
-          name="title"
-        />
-        <button type="button" onClick={addNode}>
-          Add Node
-        </button>
+        <div className="rect">
+          <h4>Add a Rectangle Node</h4>
+          <input
+            type="text"
+            placeholder="Name of the Node"
+            onChange={(e) => setName(e.target.value)}
+            name="title"
+          />
+          <button type="button" onClick={addRectangleNode}>
+            Add Node
+          </button>
+        </div>
+
+        <div className="rect">
+          <h4>Add a Square Node</h4>
+          <input
+            type="text"
+            placeholder="Name of the Node"
+            onChange={(e) => setName(e.target.value)}
+            name="title"
+          />
+          <button type="button" onClick={addSquareNode}>
+            Add Node
+          </button>
+        </div>
+        <div className="rect">
+          <h4>Add a Circle Node</h4>
+          <input
+            type="text"
+            placeholder="Name of the Node"
+            onChange={(e) => setName(e.target.value)}
+            name="title"
+          />
+          <button type="button" onClick={addCircleNode}>
+            Add Node
+          </button>
+        </div>
       </div>
       <ReactFlow
         elements={elements}
         onLoad={onLoad}
-        style={{ width: "100%", height: "80vh" }}
+        style={{ width: "75%", height: "90vh", marginLeft: "20rem" }}
+        onElementsRemove={onElementsRemove}
         onConnect={onConnect}
-        connectionLineStyle={{ stroke: "#ddd", strokeWidth: 2 }}
+        connectionLineStyle={{ stroke: "#ddd", strokeWidth: 2, animated: true }}
         connectionLineType="bezier"
         snapToGrid={true}
         snapGrid={[16, 16]}
       >
-        <Background color="#888" gap={16} />
+        <Background color="#fff" gap={12} />
         <MiniMap
           nodeColor={(n) => {
             if (n.type === "input") return "blue";
